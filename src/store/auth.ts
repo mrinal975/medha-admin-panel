5c11be2da1c0c933f286f112f0b38ddfa9e1c404
Mrinal <mrinal@nagorik.tech>
2023-06-25 10:23:17 +0600

    updae

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -7,62 +7,45 @@ interface User {
   name: string;
   token: string;
 }
-
 interface AuthState {
-  id: User | null;
-  phone: User | null;
-  email: User | null;
-  name: User | null;
+  user: User | null;
   token: User | null;
   isAuthenticated: boolean;
 }
 
 export const useAuthStore = defineStore("auth", {
   state: (): AuthState => ({
-    id: null,
-    phone: null,
-    email: null,
-    name: null,
+    user: null,
     token: null,
     isAuthenticated: false,
   }),
   getters: {
-    isLoggedIn: (state) => !!state.id,
-    getName: (state) => state.name,
-    getEmail: (state) => state.email,
-    getPhone: (state) => state.phone,
-    currentUser: (state) => state.id,
+    isAuthenticated: (state) => !!state.isAuthenticated,
+    getUser: (state) => state.user,
     getToken: (state) => state.token,
   },
   actions: {
     setUser(payload: any) {
-      this.id = payload.id;
-      this.phone = payload.phone;
-      this.name = payload.name;
-      this.email = payload.email;
       this.token = payload.access_token;
+      this.user = payload;
     },
     setAuthenticated(value: boolean) {
-      console.log("set auth", value);
+      // console.log("set auth", value);
       this.isAuthenticated = value;
-      console.log("set auth after", this.isAuthenticated);
     },
 
     login(payload: any) {
       this.setUser(payload);
       this.setAuthenticated(true);
     },
-
     logout() {
       this.setAuthenticated(false);
       this.clearAuth();
     },
     clearAuth() {
-      this.id = null;
-      this.phone = null;
-      this.name = null;
-      this.email = null;
+      this.user = null;
       this.token = null;
+      this.isAuthenticated = false;
     },
   },
 });
